97e6b2ecd75e468ace2bc1a770198fa754ff94d2
Afam Divine <38317208+definite-d@users.noreply.github.com>
2021-11-17 10:55:23 +0100

    Add files via upload

---
+++ USERGUIDE.md
@@ -0,0 +1,95 @@
+# Using Unda
+## Installation
+First, install Unda by the pip command (if you haven't already):
+````
+pip install unda
+````
+
+## Actual Usage (Super Easy Guide)
+### Prepare an object
+First, you've got to have an object which you want the undo/redo 
+functionality for. 
+For demonstration purposes, let's use this simple one:
+````python
+# Just an example object class here, you can use any object at all.
+class SomeClass:
+    def __init__(self, foo, bar):
+        self.foo = foo
+        self.bar = bar
+
+x = SomeClass('a string', 'another_string')
+y = SomeClass('some string', 'some_other_string')
+````
+### Import Unda
+Next, import the module like so:
+````python
+from unda.unda import UndaManager
+````
+The UndaManager is bascally the only interface you need for normal use.
+Next, you'll need to add objects to it.
+
+### Place your object in Unda's care
+There's two ways for adding an object to an UndaManager:
+#### Way 1 (Single):
+You can first create an UndaManager, and then add the object.
+````python
+manager = UndaManager()
+manager.add_object(key='object X', target=x)
+````
+The `key` can be literally any string. It's purpose is for referencing.
+The `target` should be the object you want to add. In our example here, that's `x`.
+
+#### Way 2 (Bulk):
+If you've got more than one object, you can add them all at once.
+First put them all in a `dict`, then pass it as a parameter when creating 
+the UndaManager:
+````python
+some_objects = {'object X': x, 'object Y': y}
+manager = UndaManager(some_objects)
+````
+
+### Using the Undo and Redo functionality
+Using `x` as a test subject here:
+````python
+UndaManager.update('object X')
+x.foo = 'The foo has changed!'
+print(x.foo)
+x = UndaManager.undo('object X')
+print(x.foo)
+x = UndaManager.redo('object X')
+print(x.foo)
+````
+Running that gives this result...:
+````text
+The foo has changed!
+A string.
+The foo has changed!
+````
+...as expected.
+
+That's basically it.
+
+### Full example code (using Way 2 to add objects):
+````python
+# Just an example object class here, you can use any object at all.
+class SomeClass:
+    def __init__(self, foo, bar):
+        self.foo = foo
+        self.bar = bar
+
+x = SomeClass('a string', 'another_string')
+y = SomeClass('some string', 'some_other_string')
+
+from unda.unda import UndaManager
+
+some_objects = {'object X': x, 'object Y': y}
+manager = UndaManager(some_objects)
+
+UndaManager.update('object X')
+x.foo = 'The foo has changed!'
+print(x.foo)
+x = UndaManager.undo('object X')
+print(x.foo)
+x = UndaManager.redo('object X')
+print(x.foo)
+````
---
+++ unda.py
@@ -0,0 +1,161 @@
+"""
+Unda Undo/Redo Algorithm Script
+Provides state-based functionality behind undo and redo of Python objects.
+"""
+from collections import deque
+from copy import copy, deepcopy
+from typing import Dict, Optional
+
+STACK_HEIGHT = 20
+
+
+class UndaObject:
+    """
+    Contains a shallow copy of the object and the deque used as an undo stack.
+    """
+    def __init__(self,
+                 target: object,
+                 undo_stack: deque = deque(maxlen = STACK_HEIGHT),
+                 redo_stack: deque = deque(maxlen = STACK_HEIGHT)):
+        self.target: object = copy(target)
+        self.undo_stack: deque = undo_stack
+        self.redo_stack: deque = redo_stack
+
+    def clear_undo_stack(self) -> None:
+        self.undo_stack.clear()
+        return None
+
+    def clear_redo_stack(self) -> None:
+        self.redo_stack.clear()
+        return None
+
+    def clear_stacks(self) -> None:
+        self.clear_undo_stack()
+        self.clear_redo_stack()
+        return None
+
+
+class UndaManager:
+    """
+    UndaManager class. Manages undo and redo operations for all objects in its care.
+    To use, pass a dict of {key: object} pairs in the "starter_objects" parameter or using add_object().
+
+    Please do not attempt to add an object by using "UndaManager.objects[key] = target",
+    as this will not work as intended. Always use the "add_object()" function instead.
+    """
+    starter_objects: Optional[Dict] = None
+
+    def __init__(self, starter_objects: Optional[Dict] = None) -> None:
+        self.starter_objects: Optional[Dict] = starter_objects
+        self.objects: Dict = {}
+        if self.starter_objects is not None:
+            self.objects.update({x: UndaObject(self.starter_objects[x]) for x in self.starter_objects})
+
+    def add_object(self, key: str, target: object) -> None:
+        """
+        Entrusts an object into the UndaManager's care.
+        Please do not attempt to add an object by using "UndaManager.objects[key] = target" (direct dictionary edits),
+        as this will not work as intended. Always use this function instead.
+        :param key: A key to reference the object. Could be anything, even the class name.
+        :param target: The object itself.
+        :return: None
+        """
+        self.objects[key] = UndaObject(target)
+        
+    def update(self, key) -> None:
+        """
+        Tells the Manager to add a deepcopied instance of an object to its deque using its key as reference.
+        This is what you should call before a change happens with the object and you want it to be undo-able,
+        i.e.: Call this when you need to save a state of the object to go back to when undo is called.
+        :param key: The key tied to the object in the UndaManager's care.
+        :return: None
+        """
+        self.objects[key].undo_stack.append(deepcopy(self.objects[key].target))
+        self.objects[key].clear_redo_stack()
+
+    def update_all(self) -> None:
+        """
+        Same as "update", but applies it to all keys.
+        :return: None
+        """
+        for key in self.objects.keys():
+            self.update(key)
+
+    def clear_all_stacks(self) -> None:
+        """
+        Calls the "clear_stacks" function for all objects.
+        :return: None
+        """
+        for key in self.objects.keys():
+            selfobjects[key].clear_stacks()
+
+    def clear_undo_stacks(self) -> None:
+        """
+        Calls the "clear_undo_stack" function for all objects.
+        :return: None
+        """
+        for key in self.objects.keys():
+            self.objects[key].clear_undo_stack()
+
+    def clear_redo_stacks(self) -> None:
+        """
+        Calls the "clear_redo_stack" function for all objects.
+        :return: None
+        """
+        for key in self.objects.keys():
+            self.objects[key].clear_redo_stack()
+
+    def undo(self, key: str, depth: int = 1) -> object:
+        """
+        The star of the show.
+        Returns (by default) the first object found in the undo deque.
+        (i.e.: a deepcopy version of the object which was added prior to the current one).
+        :param key: The key to the object.
+        :param depth: The number of states to skip with a single undo call.
+                        By default, it's 1, and should work for most uses.
+        :return: The deepcopied object. You can use this to replace the original object that needs to be undone.
+        """
+        # Clear all states above the required one.
+        self.objects[key].undo_stack = deque(
+            list(self.objects[key].undo_stack)[0:len(self.objects[key].undo_stack) - depth + 1])
+        # Get the required state
+        response = self.objects[key].undo_stack.pop()
+        # Save the state before the undo call to the redo stack.
+        self.objects[key].undo_stack.append(deepcopy(self.objects[key].target))
+        return response
+
+    def undo_all(self, depth: int = 1) -> Dict:
+        """
+        Same as undo, but applies to all objects in the UndaManager's care, and returns a dict in the format:
+        {key: object}
+        :param depth: The index of the object to be retrieved, plus 1. By default, it's 1.
+        :return: A dict in the format: {key: deepcopied object)
+        """
+        return {key: self.undo(key, depth) for key in self.objects.keys()}
+
+    def redo(self, key: str, depth: int = 1) -> object:
+        """
+        The complement of the undo function.
+        Returns (by default) the first object found in the redo deque.
+        (i.e.: a deepcopy version of the object which was added prior to the current one).
+        :param key: The key to the object.
+        :param depth: The index of the object to be retrieved, plus 1. By default, it's 1.
+        :return: The deepcopied object. You can use this to replace the original object that needs to be redone.
+        """
+        # Clear all states above the required one.
+        self.objects[key].undo_stack = deque(
+            list(self.objects[key].undo_stack)[0:len(self.objects[key].undo_stack) - depth + 1])
+        # Get the required state
+        response = self.objects[key].undo_stack.pop()
+        # Save the state before the undo call to the redo stack.
+        self.objects[key].undo_stack.append(deepcopy(self.objects[key].target))
+        return response
+
+    def redo_all(self, depth: int = 1) -> Dict:
+        """
+        Same as redo, but applies to all objects in the UndaManager's care, and returns a dict in the format:
+        {key: object}
+        :param depth: The index of the object to be retrieved, plus 1. By default, it's 1.
+        :return: A dict in the format: {key: deepcopied object)
+        """
+        return {key: self.redo(key, depth) for key in self.objects.keys()}
